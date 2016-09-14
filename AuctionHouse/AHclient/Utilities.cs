af84338f7aabfaf9bf914a6b3077364fc1f5b722
WeirdestYeti <juristheyeti@gmail.com>
2016-09-14 13:45:54 +0200

    Added class CommunicationData

--- AuctionHouse/AHclient/Utilities.cs
+++ AuctionHouse/AHclient/Utilities.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace AHclient
 {
      public static class Utilities
     {
-         public static string JsonSerialize<T>(T obj)
-         {
+        public static string JsonSerialize<T>(T obj)
+        {
             string json = Newtonsoft.Json.JsonConvert.SerializeObject(obj);
             return json;
+        }
+
+        //public static T JasonDeserialize<T>(string json, Type classType)
+        //{
+        //    var b = (Auction)Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+
+        //}
+
+
+         public static CommunicationData Decode(string json)
+         {
+             return JsonConvert.DeserializeObject<CommunicationData>(json);
          }
 
     }
